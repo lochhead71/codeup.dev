c9db218f7d1962da4701dd27d28cbe1fd074acf7
Jim Lochhead <lochhead71@gmail.com>
2016-03-10 14:52:54 -0600

    Built Auth.php, requiring the Log.php class, and building out the shells of the four required functions

---
+++ Auth.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once 'Log.php';
+
+class Auth
+{
+	public static $password = '$2y$10$SLjwBwdOVvnMgWxvTI4Gb.YVcmDlPTpQystHMO2Kfyi/DS8rgA0Fm';
+
+	public static function attempt($username, $password)
+	{
+		
+	}
+
+	public static function check()
+	{
+		
+	}
+
+	public static function user()
+	{
+		
+	}
+
+	public static function logout()
+	{
+		
+	}
+}
