5139383ad7d2a811f503176ffc988153e16ca222
Jim Lochhead <lochhead71@gmail.com>
2016-03-11 08:02:31 -0600

    Built out the functions outlined in step 3

--- Auth.php
+++ Auth.php
@@ -8,21 +8,31 @@ class Auth
 
 	public static function attempt($username, $password)
 	{
-		
+		if ($username == 'guest' &&  password_verify($Password) == self::password) {
+			$_SESSION['LOGGED_IN_USER'] = $username;
+			$log = new Log();
+			$log->info("User $username is logged in.");
+		} else {
+			$log = new Log();
+			$log->error("User $username failed to log in!");
+		}
 	}
 
 	public static function check()
 	{
-		
+		isset($_SESSION['LOGGED_IN_USER'];
 	}
 
 	public static function user()
 	{
-		
+		$username = $_SESSION['LOGGED_IN_USER'];
+		return $username;
 	}
 
 	public static function logout()
 	{
-		
+		clearSession();
+		header('location: /login.php');
+		die();
 	}
 }
