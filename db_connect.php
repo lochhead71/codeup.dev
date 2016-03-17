b0a3218c320f77311c9860a5018f79a937a4dbdf
Jim Lochhead <lochhead71@gmail.com>
2016-03-17 13:41:47 -0500

    Moved the four constant definitions to the pdo_test.php file to avoid the error message about trying to redefine constants

--- db_connect.php
+++ db_connect.php
@@ -1,10 +1,5 @@
 <?php
 
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'employees');
-define('DB_USER', 'vagrant');
-define('DB_PASS', 'vagrant');
-
 $dbc = new PDO(
 	'mysql:host=' . DB_HOST . ';dbname=' . DB_NAME,
 	DB_USER,
