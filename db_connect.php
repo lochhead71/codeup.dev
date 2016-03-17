3fd2922acb478264c0c91d9c2be5cd9f235ff6f6
Jim Lochhead <lochhead71@gmail.com>
2016-03-17 13:23:29 -0500

    Created a PDO named $dbc, included messages for exceptions, and pointed it to the employees database

---
+++ db_connect.php
@@ -0,0 +1,13 @@
+<?php
+
+define('PASSWORD', 'vagrant');
+
+$dbc = new PDO(
+	'mysql:host=127.0.0.1;dbname=employees';
+	'vagrant';
+	PASSWORD
+)
+
+$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
