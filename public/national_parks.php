9251d97ddf7fdf3e68fe165262727ac126d026db
Jim Lochhead <lochhead71@gmail.com>
2016-03-21 10:51:30 -0500

    Did some initial CSS styling of national_parks.php

--- public/national_parks.php
+++ public/national_parks.php
@@ -11,6 +11,10 @@ $stmt = $dbc->query("SELECT * FROM national_parks LIMIT 4 OFFSET {$offset}");
 
 $parks = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+$no_of_records = $dbc->query("SELECT count(id) FROM national_parks");
+
+$count = $no_of_records->fetchColumn();
+
 ?>
 
 <!DOCTYPE html>
@@ -18,17 +22,32 @@ $parks = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
 	<meta charset="UTF-8">
 	<title>U.S. National Parks</title>
+	<link rel="stylesheet" href="/css/natty_park.css">
+	<link href='https://fonts.googleapis.com/css?family=Delius+Swash+Caps|Amaranth:400,700italic|Work+Sans:300,500,800' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	<?php foreach ($parks as $park): ?>
-		<h2><?= $park['name'] ?></h2>
-		<h3><?= $park['location'] ?></h3>
-		<p><strong>Date Established: </strong><?= $park['date_established'] ?></p>
-		<p><strong>Total acreage: </strong><?= $park['area_in_acres'] ?></p><br>
-	<?php endforeach; ?>
+	<header>
+		<div>
+			<h1>America's National Parks</h1>
+		</div>
+	</header>
+		<div class='listings'>
+			<?php foreach ($parks as $park): ?>
+				<h2 class='parkName'><?= $park['name'] ?></h2>
+				<h3 class='parkLocation'><?= $park['location'] ?></h3>
+				<p><strong>Date Established: </strong><?= $park['date_established'] ?></p>
+				<p><strong>Total acreage: </strong><?= $park['area_in_acres'] ?></p><br>
+			<?php endforeach; ?>
+		</div>
+		<h3></h3>
 
-	<a href="national_parks.php?page_num=<?=$page+1 ?>">Next Page</a>
-	<a href="national_parks.php?page_num=<?=$page-1 ?>">Previous Page</a>
+	<!-- Add some logic to determine whether or not to show the next and/or previous page links. -->
+	<?php if ($page < $count/4) { ?>
+		<a href="national_parks.php?page_num=<?=$page+1 ?>">Next Page</a>
+	<?php } ?>
+	<?php if ($page > $count/$count) { ?>
+		<a href="?page_num=<?=$page-1 ?>">Previous Page</a>
+	<?php } ?>
 	
 </body>
 </html>
